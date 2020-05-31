73cc0cfefb5c38f3ca794483b73d85b38d4550bc
Lars Trieloff <trieloff@adobe.com>
2020-05-31 11:14:09 +0000

    test(post-deploy): try to create a post from deployed action

--- test/post-deploy.js
+++ test/post-deploy.js
@@ -1,5 +1,6 @@
+
 /*
- * Copyright 2019 Adobe. All rights reserved.
+ * Copyright 2020 Adobe. All rights reserved.
  * This file is licensed to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy
  * of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -11,13 +12,48 @@
  */
 
 /* eslint-env mocha */
-const assert = require('assert');
+/* eslint-disable no-unused-expressions */
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const packjson = require('../package.json');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+function getbaseurl() {
+  const namespace = 'trieloff';
+  const package = 'helix-micropub';
+  const name = packjson.name.replace('@trieloff/helix-', '');
+  let version = `${packjson.version}`;
+  if (process.env.CI && process.env.CIRCLE_BUILD_NUM && process.env.CIRCLE_BRANCH !== 'master') {
+    version = `ci${process.env.CIRCLE_BUILD_NUM}`;
+  }
+  return `api/v1/web/${namespace}/${package}/${name}@${version}`;
+}
 
 describe('Post-Deploy Tests', () => {
-  it('Service is ready for monitoring', () => {
-    assert.equal(
-      'Not yet, but I will change this line as soon as I am ready',
-      'I am ready to go on call for this',
-    );
-  });
+  it('Publish a Blog Post (draft)', async () => {
+    console.log(`Trying https://adobeioruntime.net/${getbaseurl()}/trieloff/helix-demo/master`);
+
+    await chai
+      .request('https://adobeioruntime.net/')
+      .post(`${getbaseurl()}/trieloff/helix-demo/master`)
+      .type('form')
+      .set('Authorization', `Bearer ${process.env.GITHUB_TOKEN}`)
+      .send({
+        'post-status': 'draft',
+        h: 'entry',
+        content: `# My awesome blog post
+
+Hello world from CircleCI. This is build # ${process.env.CIRCLE_BUILD_NUM}
+`,
+      })
+      .then((response) => {
+        expect(response).to.have.status(204);
+      })
+      .catch((e) => {
+        throw e;
+      });
+  }).timeout(10000);
 });
